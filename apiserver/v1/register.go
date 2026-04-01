3a833646c11a481c58c0fafbc59264436012060f
guigliu <guigliu@tencent.com>
2026-04-01 15:09:05 +0800

    feat: add register

---
+++ apiserver/v1/register.go
@@ -0,0 +1,11 @@
+package v1
+
+import "github.com/robinlg/iamlib/pkg/scheme"
+
+// GroupName is the group name use in this package.
+// If use a public domain name, need set the GroupName to service name.
+// For example: if restful path is: https://marmotedu.com/apimachinery/v1/secrets, we can set GroupName="apimachinery".
+const GroupName = "iam.api"
+
+// SchemeGroupVersion is group version used to register these objects.
+var SchemeGroupVersion = scheme.GroupVersion{Group: GroupName, Version: "v1"}
